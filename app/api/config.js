5596caff1a2c255f04c416ce25228276cbfd78e4
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-07 18:27:22 +0530

    deleted:    app/api/config.js 	deleted:    app/api/index.js 	deleted:    app/api/models/tripModel.js 	modified:   app/components/AddNew.jsx 	modified:   app/components/Main.js 	new file:   app/firebase.js 	modified:   package-lock.json 	modified:   package.json

--- app/api/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 3000
-
-export const mongoDBUrl = `mongodb+srv://abelkjohn6384:abelkjohn@cluster0.puaeyid.mongodb.net/?retryWrites=true&w=majority`
--- app/components/AddNew.jsx
+++ app/components/AddNew.jsx
@@ -2,12 +2,56 @@
 
 import React from "react";
 import { FaWindowClose } from 'react-icons/fa'
+import { ref, set } from "firebase/database";
+import { db } from "../firebase"
+import { nanoid } from 'nanoid'
+
+
 
 
 export default function AddNew(){
+    
+    const [ place, setPlace ] = React.useState('')
+    const [ location, setLocation ] = React.useState('')
+    const [ link, setLink ] = React.useState('')
+    const [ startDate, setStartDate ] = React.useState('')
+    const [ endDate, setEndDate ] = React.useState('')
+    const [ desc, setDesc ] = React.useState('')
+    const [ locationLink, setLocationLink ] = React.useState('')
+
+    
+    
+    
     function close(){
+        document.getElementById('place').value = ''
+        document.getElementById('location').value = ''
+        document.getElementById('link').value = ''
+        document.getElementById('startDate').value = ''
+        document.getElementById('endDate').value = ''
+        document.getElementById('desc').value = ''
+        document.getElementById('locationLink').value = ''
         document.getElementById('add-new').style.display = 'none'
     }
+
+    function post(e){
+        e.preventDefault()
+        const id = nanoid()
+
+        set(ref(db, 'places/' + id), {
+            place: place,
+            location: location,
+            locationlink: locationLink,
+            link: link,
+            startDate: startDate,
+            endDate: endDate,
+            desc: desc,
+            id: id,
+        });
+
+
+        close()
+    }
+    
     return (
             <div id='add-new' className="hidden bg-white add-new fixed flex-col center-element w-96 h-max border-2 shadow-lg shadow-red-400 border-red-400 rounded-xl overflow-hidden">
                 <div className=" flex justify-between bg-red-500 shadow-lg shadow-red-400/50 text-white p-3">
@@ -17,31 +61,35 @@ export default function AddNew(){
                 <div  className="gap-3 p-3  flex flex-col">
                     <div>
                         <p>Place:</p>
-                        <input className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="Name of the place..."></input>
+                        <input required id="place" className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="Name of the place..." onChange={e => setPlace(e.target.value)}></input>
                     </div>
                     <div>
                         <p>Location: </p>
-                        <input className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="State, Country..."></input>
+                        <input  id="location" required className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="State, Country..."  onChange={e => setLocation(e.target.value)}></input>
+                    </div>
+                    <div>
+                        <p>Location Image Link: </p>
+                        <input  id="locationLink" required className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="Link for img"  onChange={e => setLocationLink(e.target.value)}></input>
                     </div>
                     <div>
                         <p>Map link: </p>
-                        <input className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="Enter the location link..."></input>
+                        <input id="link" required className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="Enter the location link..."  onChange={e => setLink(e.target.value)}></input>
                     </div>
                     <div className="add-new flex justify-between">
                         <div className="flex flex-col">
                             <p>Start date:</p>
-                            <input type="date" className="w-40 p-1 border-gray-200 shadow-lg border-2"></input>
+                            <input id="startDate" required type="date" className="w-40 p-1 border-gray-200 shadow-lg border-2"  onChange={e => setStartDate(e.target.value)}></input>
                         </div>
                         <div className="flex flex-col">
                             <p>End date:</p>
-                        <input type="date" className="w-40 p-1 border-gray-200 shadow-lg border-2"></input>
+                        <input id="endDate" required type="date" className="w-40 p-1 border-gray-200 shadow-lg border-2"  onChange={e => setEndDate(e.target.value)}></input>
                         </div>
                     </div>
                     <div>
                         <p>Description</p>
-                        <input className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="Enter a description, friends, weather, activities, etc..."></input>
+                        <input id="desc" required className='w-full p-1 border-gray-200 shadow-lg border-2' placeholder="Enter a description, friends, weather, activities, etc..."  onChange={e => setDesc(e.target.value)}></input>
                     </div>
-                    <button className="my-1 bg-red-500 text-white w-40 p-2 rounded-lg mx-auto shadow-lg shadow-red-500/50 border-red-400 border-1">Add Trip</button>
+                    <button className="my-1 bg-red-500 text-white w-40 p-2 rounded-lg mx-auto shadow-lg shadow-red-500/50 border-red-400 border-1" onClick={e => post(e)}>Add Trip</button>
                 </div>
         </div>
     )
